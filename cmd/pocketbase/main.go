89b472d30445325a125dab593eb901142c4eb51d
Ovi1kanobe <94891720+Ovi1kanobe@users.noreply.github.com>
2025-06-28 22:15:49 -0400

    Update main.go

--- cmd/pocketbase/main.go
+++ cmd/pocketbase/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/pocketbase/pocketbase"
 
-	_ "github.com/pluja/pocketbase/migrations"
+	_ "github.com/Ovi1kanobe/pocketbase-go-sdk/migrations"
 )
 
 func main() {
